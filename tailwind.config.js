fb5b08f5dd963823da9036de0e20bcf096d61a82
Satish Rathod <satishrathd.dev@gmail.com>
2025-06-18 20:23:44 +0530

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,10 @@
 /** @type {import('tailwindcss').Config} */
 export default  {
   darkMode: "class",
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "*.{js,ts,jsx,tsx,mdx}"],
+  content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+      ],
   theme: {
     extend: {
       colors: {
